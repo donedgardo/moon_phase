7114dee7932819fbbf7c217fba52d2eb79eaa1bc
Edgardo Carreras <me@edgardocarreras.com>
2023-09-04 03:32:44 -0400

    Add tailwind support

---
+++ src/bin/build-css.rs
@@ -0,0 +1,18 @@
+use std::process::{Command};
+
+fn main() {
+    let output = Command::new("npx")
+        .arg("tailwindcss")
+        .arg("-i")
+        .arg("./src/input.css")
+        .arg("-o")
+        .arg("./static/output.css")
+        .output()
+        .expect("failed to execute process");
+    println!(
+        "CSS Build Finished{}{}{}",
+        output.status,
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr)
+    );
+}
