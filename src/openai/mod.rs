4bc471884fdd2ea37726390bba78158607959d9f
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-11-14 01:40:00 -0400

    Add get_embeddings feature

--- src/openai/mod.rs
+++ src/openai/mod.rs
@@ -1,22 +1,26 @@
 use actix::dev::ToEnvelope;
 use actix::{Actor, Addr, Handler, Message};
 use actix_web_actors::ws::WebsocketContext;
-use leptos::{view, IntoAttribute, IntoView};
+use leptos::{view, IntoAttribute};
 use reqwest::Client;
 use serde_derive::{Deserialize, Serialize};
 use tokio_stream::StreamExt;
 
-//Open ai response
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct OpenAiApiResponse {
+pub struct GenerateApiResponse {
     model: String,
     created_at: String,
     response: String,
     done: bool,
 }
 
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct EmbeddingApiResponse {
+    embedding: Vec<f64>
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct OpenAiRequestPayload {
+pub struct GenerateRequestPayload {
     model: String,
     prompt: String,
 }
@@ -37,7 +41,7 @@ where
     T::Context: ToEnvelope<T, TextMessage>,
 {
     let client = Client::new();
-    let payload = OpenAiRequestPayload {
+    let payload = GenerateRequestPayload {
         model: model.to_string(),
         prompt: prompt.to_string(),
     };
@@ -48,7 +52,7 @@ where
                 let json_str = std::str::from_utf8(&bytes).unwrap_or_default();
                 for line in json_str.lines() {
                     let target = format!("beforeend:#ai-msg-{}", id.to_string());
-                    let api_response: OpenAiApiResponse =
+                    let api_response: GenerateApiResponse =
                         serde_json::from_str(line).unwrap_or_default();
                     let formatted_response = api_response.response.replace("\n", "<br>");
                     let msg_part = leptos::ssr::render_to_string(move |cx| {
@@ -63,4 +67,47 @@ where
         }
     }
     Ok(())
+}
+
+pub async fn get_embeddings(content: &str, host: &str, model: &str) -> Result<Vec<f64>, reqwest::Error> {
+    if content.is_empty() {
+        return Ok(vec![]);
+    }
+    let client = Client::new();
+    let payload = GenerateRequestPayload {
+        model: model.to_string(),
+        prompt: content.to_string(),
+    };
+    let mut response = client.post(format!("{host}/api/embeddings"))
+        .json(&payload).send().await?;
+    match response.json::<EmbeddingApiResponse>().await {
+        Ok(response) => Ok(response.embedding),
+        _ => Ok(vec![])
+    }
+}
+
+#[cfg(test)]
+mod openai_test {
+    use super::*;
+    #[tokio::test]
+    async fn get_embeddings_returns_empty() {
+        let mut server = mockito::Server::new();
+        let host = server.url();
+        let embeddings = get_embeddings("", &host, "dndai").await.unwrap();
+        assert_eq!(embeddings.len(), 0);
+    }
+    #[tokio::test]
+    async fn get_embeddings_returns_vec() {
+        let mut server = mockito::Server::new();
+        let host = server.url();
+        let mock = server.mock("POST", "/api/embeddings")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body("{\"embedding\": [0.5]}")
+            .create();
+
+        let embeddings = get_embeddings("hello!", &host, "dndai").await.unwrap();
+        mock.assert();
+        assert_eq!(embeddings.len(), 1);
+    }
 }
